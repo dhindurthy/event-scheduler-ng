eaafff15daec43e056d12a9d6a1cf14db24e0553
dhindurthy <dhirajindurthy@gmail.com>
2017-02-14 17:08:06 -0600

    Added Routing to the app for navigation

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { EventComponent } from './event/event.component';
+import { EventAddComponent } from './event-add/event-add.component';
+
+/**
+ * [myRoutes Place where the routes to 
+ * different pages need to be mentioned 
+ * and the default page to redirect as well]
+ * @type {Routes}
+ */
+const myRoutes: Routes = [
+  { path: '', redirectTo: '/event', pathMatch: 'full' },
+  { path: 'event',  component: EventComponent },
+  { path: 'event-add',  component: EventAddComponent },
+];
+
+/**
+ * Registering the Router Module
+ */
+@NgModule({
+  imports: [ RouterModule.forRoot(myRoutes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,41 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import {enableProdMode} from '@angular/core';
 import { EventComponent } from './event/event.component';
+import { EventAddComponent } from './event-add/event-add.component';
+import { EventService } from './event.service';
 
+import { AppRoutingModule } from './app-routing.module';
+
+/**
+ * [enableProdMode To enable the production mode]
+ */
 enableProdMode();
+
+/**
+ * [NgModule]
+ *  Declarations of all components, 
+ *  Imports(of modules provided by angular), 
+ *  Providers for services, 
+ *  Bootstrapping the AppComponent are done here
+ */
 @NgModule({
   declarations: [
     AppComponent,
-    EventComponent
+    EventComponent,
+    EventAddComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [EventService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
