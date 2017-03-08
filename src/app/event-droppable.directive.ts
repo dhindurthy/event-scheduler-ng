05edc0ea26e545089670cc29190e5bc97b20f9a4
dhindurthy <dhirajindurthy@gmail.com>
2017-03-08 12:25:07 -0600

    Update to droppable directive

--- src/app/event-droppable.directive.ts
+++ src/app/event-droppable.directive.ts
@@ -1,7 +1,7 @@
 import { Output, ElementRef, EventEmitter, Input, HostListener, Directive, HostBinding } from '@angular/core';
 
 export interface DropTargetOptions {
-  zone?: string;
+  dragdroparea?: string;
 }
 
 @Directive({
@@ -9,44 +9,63 @@ export interface DropTargetOptions {
 })
 
 export class EventDroppableDirective {
+  /**
+   * [Input Property that allows us to specify the dragdroparea
+   * like [appEventDroppable]="{dragdroparea:'another'}"] if required
+   */
   @Input()
-  set myDropTarget(options: DropTargetOptions) {
+  set appEventDroppable(options: DropTargetOptions) {
     if (options) {
       this.options = options;
     }
   }
   
+  /**
+   * [Output Property which will emit whenever 
+   * the user drops something which has a prefix 
+   * for usage purpose]
+   * 
+   */
   @Output('dropSection') drop = new EventEmitter();
 
   constructor(private el: ElementRef) { }
   
   private options: DropTargetOptions = {};
   
+  /**
+   * [HostListener Checks for the presence of the custom MIME type]
+   * 
+   */
   @HostListener('dragover', ['$event'])
   onDragOver(event) {
 
     this.el.nativeElement.classList.add('event-drag-action');
 
-    const { zone = 'zone'} = this.options;
-    if (event.dataTransfer.types.indexOf(`application/x-${zone}`) >= 0) {
+    const { dragdroparea = 'dragdroparea'} = this.options;
+    if (event.dataTransfer.types.indexOf(`application/x-${dragdroparea}`) >= 0) {
       event.preventDefault();
     }
   }
   
+  /**
+   * [HostListener Handles the drop event and emits the 
+   * 'dropSection' event with the data associated with the drag operation]
+   */
   @HostListener('drop', ['$event'])
   onDrop(event) {
 
     this.el.nativeElement.classList.remove('event-drag-action');
 
-    const { zone = 'zone' } = this.options;
-    const data = JSON.parse(event.dataTransfer.getData(`application/x-${zone}`));
-    data.section = event.target.id;
-    let itemStartsAt = document.getElementById(data.id).getAttribute('group');
+    const { dragdroparea = 'dragdroparea' } = this.options;
+    const eventData = JSON.parse(event.dataTransfer.getData(`application/x-${dragdroparea}`));
+    eventData.section = event.target.id;
+    let itemStartsAt = document.getElementById(eventData.id).getAttribute('group');
     let targetAreaId = event.target.id;
     let itemEndsIn = event.target.getAttribute('group');
     console.log('item started in: ' +itemStartsAt);
     console.log("target area's id: " + targetAreaId);
     console.log('item ended in: ' + itemEndsIn);
+
     /**
      * itemEndsIn //should be available and non-empty
      * targetAreaId //should be available and non-empty
@@ -54,14 +73,13 @@ export class EventDroppableDirective {
      * itemStartsAt !== itemEndsIn
      */
     if (itemEndsIn && itemEndsIn !== '' && targetAreaId && targetAreaId !== '' && itemStartsAt !== itemEndsIn){
-        this.drop.next(data);
+        this.drop.next(eventData);
     } else {
-      document.getElementById(data.id).classList.add('drag-action-disabled');
+      document.getElementById(eventData.id).classList.add('drag-action-disabled');
       setTimeout(function() {
-        document.getElementById(data.id).classList.remove('drag-action-disabled');
+        document.getElementById(eventData.id).classList.remove('drag-action-disabled');
       }, 2000);
     }
-    //this.drop.next(data);
   }
 
   @HostListener('dragenter', ['$event'])
