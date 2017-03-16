1cd05c712328cd2696982161a210a719a42c2ea0
dhindurthy <dhirajindurthy@gmail.com>
2017-03-15 20:22:41 -0500

    Update to locale files to include new keywords

--- src/app/localization/localize-en.ts
+++ src/app/localization/localize-en.ts
@@ -3,8 +3,31 @@
 export const LANG_EN_NAME = 'en';
 
 export const LANG_EN_TRANS = {
-    'search': 'Search',
-    'list': 'List',
+    'search':'Search',
+    'list':'List',
     'add':'Add',
-    'help':'Help'
+    'help':'Help',
+    'name-placeholder':'Name of Event',
+    'description-placeholder':'Description of Event',
+    'people-placeholder':'People joining you',
+    'date-placeholder':'Date of Event',
+    'time-placeholder':'Time',
+    'address-placeholder':'Location of Event',
+    'name-required-error':'Name is required',
+    'description-required-error':'Description is required',
+    'people-required-error':'People are required',
+    'date-required-error':'Date is required',
+    'time-required-error':'Time is required',
+    'address-required-error':'Address is required',
+    'back-button':'Back',
+    'save-button':'Save It',
+    'clear-button':'Clear It',
+    'event-add-message':'Event is now added'
 };
+
+// 'namePlaceholder':'Event Name',
+//     'description-placeholder':'Description of Event',
+//     'people-placeholder':'People Joining you',
+//     'date-placeholder':'Date of Event',
+//     'time-placeholder':'Time',
+//     'address-placeholder':'Location of Event',
--- src/app/localization/localize-es.ts
+++ src/app/localization/localize-es.ts
@@ -6,5 +6,22 @@ export const LANG_ES_TRANS = {
     'search': 'Buscar',
     'list': 'Lista',
     'add':'Añadir',
-    'help':'Ayuda'
+    'help':'Ayuda',
+    'name-placeholder':'Nombre del evento',
+    'description-placeholder':'Descripción del Evento',
+    'people-placeholder':'Personas que se unen a usted',
+    'date-placeholder':'Fecha del evento',
+    'time-placeholder':'Hora',
+    'address-placeholder':'Ubicación del Evento',
+
+    'name-required-error':'Se requiere el nombre',
+    'description-required-error':'Descripción necesaria',
+    'people-required-error':'Se requiere gente',
+    'date-required-error':'Se requiere una fecha',
+    'time-required-error':'Se requiere tiempo',
+    'address-required-error':'La dirección es necesaria',
+    'back-button':'Espalda',
+    'save-button':'Guardarlo',
+    'clear-button':'Limpialo',
+    'event-add-message':'Se añade ahora el evento'
 };
